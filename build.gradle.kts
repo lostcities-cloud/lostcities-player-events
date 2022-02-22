37065806ee3333909e0cb48a5bc086dd6601453a
Derek <contact@dereknelson.io>
2022-02-21 20:19:49 -0500

    Renaming player events debug and adding debug flag to build gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,12 @@ tasks.withType<KotlinCompile> {
 	}
 }
 
+tasks.bootRun {
+	if (project.hasProperty("debug_jvm")) {
+		jvmArgs("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005")
+	}
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
