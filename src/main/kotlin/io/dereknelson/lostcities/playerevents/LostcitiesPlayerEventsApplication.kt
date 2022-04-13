6c43c06976311fc96dfd58b219faa970ae5b7d3b
Derek Nelson <dereknelson01@gmail.com>
2022-04-13 02:57:46 +0000

    Updating name

---
+++ src/main/kotlin/io/dereknelson/lostcities/playerevents/LostcitiesPlayerEventsApplication.kt
@@ -0,0 +1,19 @@
+package io.dereknelson.lostcities.playerevents
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.amqp.rabbit.annotation.EnableRabbit
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+@EnableRabbit
+class LostcitiesPlayerEventsApplication
+
+@Bean
+fun mapper() = jacksonObjectMapper().registerKotlinModule()
+
+fun main(args: Array<String>) {
+    runApplication<LostcitiesPlayerEventsApplication>(*args)
+}
