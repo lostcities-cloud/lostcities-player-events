b6cb784ab9f38bb92faf82442d534d9e56fab356
Derek Nelson <me@derekenelson.io>
2024-10-02 09:14:27 -0400

    adding configurable postgres variables

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.+"
+    id("org.springframework.boot") version "3.1.+"
     id("io.spring.dependency-management") version "1.1.+"
     // id("org.graalvm.buildtools.native") version "0.10.+"
     id("org.jetbrains.dokka") version "1.6.10"
