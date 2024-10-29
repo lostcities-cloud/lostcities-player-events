d150c8d30b364060ec2acf16b4e65e0e555d3301
Derek Nelson <me@derekenelson.io>
2024-10-29 18:18:04 -0400

    Upgrading models

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,8 @@ dependencies {
         implementation(project(":lostcities-common"))
         implementation(project(":lostcities-models"))
     } else {
-        implementation("io.dereknelson.lostcities-cloud:lostcities-common:0.0.2")
-        implementation("io.dereknelson.lostcities-cloud:lostcities-models:0.0.0")
+        implementation("io.dereknelson.lostcities-cloud:lostcities-common:0.0.3")
+        implementation("io.dereknelson.lostcities-cloud:lostcities-models:0.0.2")
     }
 
     implementation("org.springframework.boot:spring-boot-devtools")
