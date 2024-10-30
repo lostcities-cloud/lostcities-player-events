6bfa6d2c4d5df50a9bc50da4467e8bd157ba039c
Derek Nelson <me@derekenelson.io>
2024-10-30 00:42:04 -0400

    Adding jacoco to run.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    jacoco
     id("org.springframework.boot") version "3.3.+"
     id("org.owasp.dependencycheck") version "11.0.0"
     id("com.github.rising3.semver") version "0.8.2"
@@ -169,4 +170,14 @@ dependencyCheck {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
 
