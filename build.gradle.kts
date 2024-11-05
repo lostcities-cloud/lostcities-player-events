a7077c7a1366460c7bcd0c97ac0d347f957b6d5e
nelson54 <me@dereknelson.io>
2024-11-04 19:17:45 -0500

    Adding turns to turn change.

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
         implementation(project(":lostcities-models"))
     } else {
         implementation("io.dereknelson.lostcities-cloud:lostcities-common:0.0.3")
-        implementation("io.dereknelson.lostcities-cloud:lostcities-models:0.0.2")
+        implementation("io.dereknelson.lostcities-cloud:lostcities-models:0.0.3")
     }
 
     implementation("org.springframework.boot:spring-boot-devtools")
